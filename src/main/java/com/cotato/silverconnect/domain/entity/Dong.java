035b697bd47315064815292f30ee99f2b1867b72
qjvk2880 <qjvk2880@gmail.com>
2023-06-30 21:57:56 +0900

    Feat : 지역 엔티티 생성 완료

---
+++ src/main/java/com/cotato/silverconnect/domain/entity/Dong.java
@@ -0,0 +1,37 @@
+package com.cotato.silverconnect.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dong")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Dong {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dong_id")
+    private Long id;
+
+    private String name;
+    private double latitude;
+    private double longitude;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gu_id")
+    private Gu gu;
+
+
+    @Builder
+    public Dong(String name, double latitude, double longitude, Gu gu) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.gu = gu;
+    }
+}
