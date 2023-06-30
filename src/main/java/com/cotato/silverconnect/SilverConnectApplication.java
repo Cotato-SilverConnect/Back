01c84794ced59f2ee171619776c5c46a799e8020
qjvk2880 <qjvk2880@gmail.com>
2023-06-30 20:20:22 +0900

    Initial Commit

---
+++ src/main/java/com/cotato/silverconnect/SilverConnectApplication.java
@@ -0,0 +1,13 @@
+package com.cotato.silverconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilverConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cotato/silverconnect/SilverConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotato.silverconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
